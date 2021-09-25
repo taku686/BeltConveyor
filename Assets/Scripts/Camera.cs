e9e65566fe71015205603184cb79c0ae3e60f8b0
Taku <takunoshin123456789@gmai.com>
2021-09-26 01:16:12 +0900

    InitCommit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 setoff;
+    [SerializeField]
+    GameObject player;
+    private void Start()
+    {
+        setoff = this.transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position = player.transform.position + setoff;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float moveSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var hori = Input.GetAxis("Horizontal");
+        var vert = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(hori, 0, vert) * moveSpeed;
+        // transform.position += new Vector3(hori, 0, vert) * moveSpeed * Time.fixedDeltaTime;
+    }
+}
